656c891d58b5be788863ff3ae72be8521e19d876
majid <majid@pc.com>
2021-08-24 19:07:29 +0300

    Modefy restart massage

--- bikeshare.py
+++ bikeshare.py
@@ -180,7 +180,7 @@ def main():
         trip_duration_stats(df)
         user_stats(df, city)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nYou can restart if would? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
